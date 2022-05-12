9c7718b4b34eaa4cab10bb7b097f5fb589b47230
turna <nzttsnm@gmail.com>
2022-05-12 23:51:58 +0600

    changed port

--- index.js
+++ index.js
@@ -30,5 +30,5 @@ app.use('/appointments', appointments);
 // app.use('/dataentry', dataentry);
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
