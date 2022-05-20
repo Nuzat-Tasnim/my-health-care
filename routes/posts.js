bb2143abe552f7e81ee7772e11aecd4f93e599be
turna <nzttsnm@gmail.com>
2022-05-21 00:23:15 +0600

    Fixed /send post

--- routes/posts.js
+++ routes/posts.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const router = express.Router();
-const {Post, createPost, getPost, getPosts} = require("../models/post");
+const auth = require("../middleware/auth");
+const {getUserById} = require("../models/user");
+const {createPost, getPost, getPosts} = require("../models/post");
 
-router.get("/feed", async (req, res) => {
+router.get("/feed", auth, async (req, res) => {
     let posts = await getPosts();
     res.send(posts);
 });
 
-router.post("/send", async (req, res) => {
-    let post = await createPost(req.body.name, req.body.bloodType, req.body.text, req.body.contact);
+router.post("/send", auth, async (req, res) => {
+    let user = getUserById(req.user._id);
+
+    let post = await createPost(user.name, req.body.bloodType, req.body.text, req.body.contact);
     if(!post) return res.status(500).send("Soemthing went wrong! PLease try again later.");
 
     res.send(post)
 });
 
-router.get("/:postId", async (req, res) => {
+router.get("/:postId", auth, async (req, res) => {
     let post = await getPost(req.params.postId);
     if(!post) return res.status(404).send("Post not found.");
 
